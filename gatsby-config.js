75d0dca752d005d9e7b8e2f3721ec41e37c1106b
AAndreazza <alanaandreazza26@hotmail.com>
2024-01-17 10:21:34 -0300

    alteracao de favicon e adicao de mais duas experiencias

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
         {
             resolve: 'gatsby-plugin-manifest',
             options: {
-                icon: 'src/images/logoMaior.png',
+                icon: 'src/images/logoSemFundo2x.png',
             }
         }
     ],
