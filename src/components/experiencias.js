d19805900ed37a0302faceb76a2e356f1ebea3a2
AAndreazza <alanaandreazza26@hotmail.com>
2024-01-17 11:00:33 -0300

    alteracao de experiencias por ordem alfabetica

--- src/components/experiencias.js
+++ src/components/experiencias.js
@@ -10,7 +10,7 @@ const Experiencias = () => {
             <div className='mx-auto max-w-2xl text-center'>
             {/* ANALISAR FONTE */}
             <h2 className='text-3xl font-times tracking-tight text-slate-100 sm:text-2xl'>Psicóloga clínica com experiência em:</h2>
-            <p className='mx-auto mt-4 lg:px-0 px-5 max-w-xl font-light text-lg leading-8 text-slate-100 whitespace-pre-wrap'>Ansiedade    Aposentadoria    Autoconhecimento    Burnout    Demências    Depressão    Diagnóstico Diferencial    Fobias    Gestão de Pessoas    Liderança    Luto    Relacionamentos   Transtorno Bipolar     <br/>Transtorno de Ansiedade Generalizada (TAG)    Transtorno de Pânico    Transtorno de Déficit de Atenção e Hiperatividade (TDAH)    <br/>Transtorno de Estresse Pós-Traumático    Compulsão Alimentar</p>
+            <p className='mx-auto mt-4 lg:px-0 px-5 max-w-xl font-light text-lg leading-8 text-slate-100 whitespace-pre-wrap'>Ansiedade    Aposentadoria    Autoconhecimento    Burnout    <br/>Compulsão Alimentar    Demências    Depressão    Diagnóstico Diferencial    Fobias    Gestão de Pessoas    Liderança    Luto    Relacionamentos     Transtorno de Ansiedade Generalizada (TAG)   Transtorno Bipolar    Transtorno de Déficit de Atenção e Hiperatividade (TDAH)    <br/>Transtorno de Estresse Pós-Traumático    Transtorno de Pânico</p>
             <div className='mt-8 flex items-center justify-center gap-x-6'>
                 <Link to={getWhatsUrl(CTA_WHATSAPP_MENSAGENS.ctaGeral)} className='rounded-md bg-[#78dbbf] drop-shadow-2xl px-5 py-3 font-times font-semibold text-black shadow-sm hover:bg-[#b1ffe9] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#b1ffe9]'>
                     Entre em contato
