7c6c73ea44782446c740554513f3c79843e9e17d
AAndreazza <alanaandreazza26@hotmail.com>
2024-03-15 09:27:30 -0300

    alteracao de endereco

--- src/components/contato.js
+++ src/components/contato.js
@@ -19,7 +19,7 @@ const Contato = () => {
                         <path stroke-linecap='round' stroke-linejoin='round' d='M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z' />
                     </svg>
                     </dt>
-                    <dd>R. Ângelo Dias, 220 - Sala 707 - Centro <br/>Blumenau - SC, 89010-001</dd>
+                    <dd>R. Presidente Getúlio Vargas, 196 - Sala 506 <br/> Centro - Blumenau, SC</dd>
                 </div>
                 <div className='flex gap-x-4'>
                     <dt className='flex-none'>
