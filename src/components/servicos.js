7b051a5726384612ca5d88caee3a67bb93e8bcbf
AAndreazza <alanaandreazza26@hotmail.com>
2024-05-23 07:30:04 -0300

    alterando texto dos servicos

--- src/components/servicos.js
+++ src/components/servicos.js
@@ -20,12 +20,12 @@ const Servicos = () => {
 
                     <div className='bg-fundoEspecifico+ lg:ml-48 -mb-8 lg:mb-8 rounded-lg lg:l-12 lg:w-96 mx-auto'>
                         <dt className='mt-5 text-center text-xl font-light text-white'>Avaliação Psicológica</dt>
-                        <dd className='mx-4 my-5 text-center font-light text-slate-100 px-2'>Avaliação psicológica para cirurgia bariátrica, vasectomia, laqueadura e histerectomia. <br/> Avaliação psicológica para concursos públicos <br/>e processos seletivos.</dd>
+                        <dd className='mx-4 my-5 text-center font-light text-slate-100 px-2'>Avaliação psicológica para cirurgia bariátrica, vasectomia, laqueadura e histerectomia. Acompanhamento pré e pós-operatório. Avaliação psicológica para concursos públicos e processos seletivos.</dd>
                     </div>
 
                     <div className='bg-fundoEspecifico+ lg:mr-48 mb-8 rounded-lg lg:l-12 lg:w-96 mx-auto'>
                         <dt className='mt-5 text-center text-xl font-light text-white'>Avaliação Neuropsicológica</dt>
-                        <dd className='mx-5 lg:mx-3 my-4 text-center font-light text-slate-100 px-2'>A avaliação neuropsicológica busca investigar aspectos cognitivos, emocionais e comportamentais, por meio de entrevistas e testes. Pode ser realizada quando há suspeita de algum quadro ou transtorno, auxiliando no diagnóstico e no planejamento de um tratamento eficaz.</dd>
+                        <dd className='mx-4 lg:mx-3 my-4 text-center font-light text-slate-100 px-2'>Busca investigar aspectos cognitivos, emocionais e comportamentais, por meio de entrevistas e testes. Pode ser realizada quando há suspeita de algum quadro ou transtorno, auxiliando no diagnóstico e no planejamento de um tratamento eficaz.</dd>
                     </div>
                 </div>
             </div>
