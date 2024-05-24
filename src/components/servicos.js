e4152f36a7a0eaf2757716c086af92ea736ff4cd
AAndreazza <alanaandreazza26@hotmail.com>
2024-05-24 10:57:18 -0300

    alteracao de imagem e texto servico adulto

--- src/components/servicos.js
+++ src/components/servicos.js
@@ -10,7 +10,7 @@ const Servicos = () => {
                 <div className='mt-12 grid grid-cols-1 gap-y-16 sm:grid-cols-1 lg:grid-cols-2'>
                     <div className='bg-fundoEspecifico+ lg:ml-48 -mb-8 rounded-lg lg:l-12 lg:w-96 mx-auto'>
                         <dt className='mt-5 text-center text-xl font-light text-white'>Terapia para Adultos</dt>
-                        <dd className='mx-4 my-5 text-center font-light text-slate-100 px-2'>Atendimento psicológico presencial ou on-line com base na Terapia Cognitivo-Comportamental, Terapia do Esquema e Neuropsicologia. As sessões ocorrem semanal ou quinzenalmente, com duração de 50 minutos, sendo realizadas no consultório em Blumenau/SC ou por meio de vídeo chamada.</dd>
+                        <dd className='mx-4 my-5 text-center font-light text-slate-100 px-2'>Atendimento psicológico presencial ou on-line com base na Terapia Cognitivo-Comportamental, Terapia do Esquema e Neuropsicologia. As sessões ocorrem semanal ou quinzenalmente, com duração de 50 minutos, no consultório em Blumenau/SC ou por meio de vídeo chamada.</dd>
                     </div>
 
                     <div className='bg-fundoEspecifico+ lg:mr-48 -mb-8 rounded-lg lg:l-12 lg:w-96 mx-auto'>
